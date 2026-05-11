c0b656b50e19f2bc2f586a6c993e6aeb40dad6ea
Risto Tõldsep <r.toldsep@gmail.com>
2026-05-11 12:25:39 +0300

    Update banner text to show apartment ready announcement

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -39,7 +39,7 @@
     <div class="bg-primary-700">
       <div class="banner-wrapper mx-auto px-5 py-2">
         <p class="marquee text-sm uppercase text-primary-100 text-center">
-          {{$t('Limiteeritud aja pakkumisena panipaik TASUTA! *Kehtib eluruumi korteritele')}}
+          {{$t('NÄIDISKORTER ON VALMIS!')}}
         </p>
       </div>
     </div>
